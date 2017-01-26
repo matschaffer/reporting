f3a842d910ad4a4e342359f8071172158c8f88be
Mat Schaffer <mat@schaffer.me>
2017-01-26 23:06:02 +0900

    A metabase box

---
+++ scripts/install_metabase.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+VERSION=0.22.1
+JAR=/opt/metabase-${VERSION}.jar
+
+if [[ -f ${JAR} ]]; then
+  echo "Metabase ${VERSION} already installed."
+  exit 0
+fi
+
+curl -sL "http://downloads.metabase.com/v${VERSION}/metabase.jar" > "${JAR}"
+
+ln -sf "${JAR}" /opt/metabase.jar
+
+if /sbin/status metabase | /bin/grep -q running; then
+    /sbin/restart metabase
+else
+    /sbin/start metabase
+fi
