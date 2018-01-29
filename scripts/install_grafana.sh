ee307ab8176b1fc05b8290d239a8afc0c2dab920
Mat Schaffer <mat@schaffer.me>
2018-01-29 13:07:50 +0900

    Also install grafana

---
+++ scripts/install_grafana.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+if rpm -q grafana; then
+  echo "Grafana already installed."
+  exit 0
+fi
+
+yum install -y https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-4.6.3-1.x86_64.rpm
+
+/sbin/chkconfig --add grafana-server
+service grafana-server start
