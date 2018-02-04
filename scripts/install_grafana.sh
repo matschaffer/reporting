9e302366ae7c7446236f3f1165488aa5916fce32
Mat Schaffer <mat@schaffer.me>
2018-02-04 13:31:59 +0900

    Worldmap plugin

--- scripts/install_grafana.sh
+++ scripts/install_grafana.sh
@@ -10,6 +10,7 @@ fi
 yum install -y https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-4.6.3-1.x86_64.rpm
 
 /usr/sbin/grafana-cli plugins install petrslavotinek-carpetplot-panel
+/usr/sbin/grafana-cli plugins install grafana-worldmap-panel
 
 /sbin/chkconfig --add grafana-server
 /sbin/service grafana-server start
