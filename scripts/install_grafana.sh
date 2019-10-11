1034b9605096c94b6a8788a42b48db480b4897fa
Mat Schaffer <mat@schaffer.me>
2019-10-11 13:21:47 +0900

    Upgrade grafana and try to configure an image bucket

--- scripts/install_grafana.sh
+++ scripts/install_grafana.sh
@@ -7,7 +7,7 @@ if rpm -q grafana; then
   exit 0
 fi
 
-yum install -y https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-5.3.2-1.x86_64.rpm
+yum install -y https://dl.grafana.com/oss/release/grafana-6.4.2-1.x86_64.rpm
 
 /usr/sbin/grafana-cli plugins install petrslavotinek-carpetplot-panel
 /usr/sbin/grafana-cli plugins install grafana-worldmap-panel
