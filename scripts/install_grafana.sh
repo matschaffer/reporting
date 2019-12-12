83c057d1f3065c79dd0132fc90c4b54cce702005
Mat Schaffer <mat@schaffer.me>
2019-12-12 10:51:05 +0900

    Grafana version bumps

--- scripts/install_grafana.sh
+++ scripts/install_grafana.sh
@@ -7,12 +7,12 @@ if rpm -q grafana; then
   exit 0
 fi
 
-yum install -y https://dl.grafana.com/oss/release/grafana-6.4.2-1.x86_64.rpm
+yum install -y https://dl.grafana.com/oss/release/grafana-6.5.2-1.x86_64.rpm
 
 /usr/sbin/grafana-cli plugins install petrslavotinek-carpetplot-panel
 /usr/sbin/grafana-cli plugins install grafana-worldmap-panel
 
-/usr/sbin/grafana-cli --pluginUrl https://github.com/panodata/grafana-map-panel/releases/download/0.6.4/grafana-map-panel-0.6.4.zip plugins install grafana-worldmap-panel-ng
+/usr/sbin/grafana-cli --pluginUrl https://github.com/panodata/grafana-map-panel/releases/download/0.7.0/grafana-map-panel-0.7.0.zip plugins install grafana-worldmap-panel-ng
 
 /sbin/chkconfig --add grafana-server
 /sbin/service grafana-server start
