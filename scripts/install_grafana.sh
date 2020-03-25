0894ca6ba91d383c5393206bbefb51dcbb300848
Rob Oudendijk <rob@yr-design.biz>
2020-03-25 18:01:32 +0900

    Updated to V0.9.0

--- scripts/install_grafana.sh
+++ scripts/install_grafana.sh
@@ -12,7 +12,7 @@ yum install -y https://dl.grafana.com/oss/release/grafana-6.5.2-1.x86_64.rpm
 /usr/sbin/grafana-cli plugins install petrslavotinek-carpetplot-panel
 /usr/sbin/grafana-cli plugins install grafana-worldmap-panel
 
-/usr/sbin/grafana-cli --pluginUrl https://github.com/panodata/grafana-map-panel/releases/download/0.7.0/grafana-map-panel-0.7.0.zip plugins install grafana-worldmap-panel-ng
+/usr/sbin/grafana-cli --pluginUrl https://github.com/panodata/grafana-map-panel/releases/download/0.9.0/grafana-map-panel-0.9.0.zip plugins install grafana-worldmap-panel-ng
 
 /sbin/chkconfig --add grafana-server
 /sbin/service grafana-server start
