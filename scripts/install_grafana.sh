e80b25c0e560e417de53c15bcd980af064824855
Rob Oudendijk <rob@yr-design.biz>
2020-03-27 18:04:21 +0900

    Update install_grafana.sh
    Update shell to install Grafana 6.7.1

--- scripts/install_grafana.sh
+++ scripts/install_grafana.sh
@@ -7,7 +7,8 @@ if rpm -q grafana; then
   exit 0
 fi
 
-yum install -y https://dl.grafana.com/oss/release/grafana-6.5.2-1.x86_64.rpm
+wget https://dl.grafana.com/oss/release/grafana-6.7.1-1.x86_64.rpm
+sudo yum install grafana-6.7.1-1.x86_64.rpm
 
 /usr/sbin/grafana-cli plugins install petrslavotinek-carpetplot-panel
 /usr/sbin/grafana-cli plugins install grafana-worldmap-panel
