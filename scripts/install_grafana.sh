2b225fcee4c9a7b0d1bf21e25535d67b7a824dae
Rob Oudendijk <rob@yr-design.biz>
2020-03-27 20:10:05 +0900

    Added Google sheets as data source

--- scripts/install_grafana.sh
+++ scripts/install_grafana.sh
@@ -12,6 +12,7 @@ sudo yum install grafana-6.7.1-1.x86_64.rpm
 
 /usr/sbin/grafana-cli plugins install petrslavotinek-carpetplot-panel
 /usr/sbin/grafana-cli plugins install grafana-worldmap-panel
+/usr/sbin/grafana-cli plugins install grafana-googlesheets-datasource
 
 /usr/sbin/grafana-cli --pluginUrl https://github.com/panodata/grafana-map-panel/releases/download/0.9.0/grafana-map-panel-0.9.0.zip plugins install grafana-worldmap-panel-ng
 
