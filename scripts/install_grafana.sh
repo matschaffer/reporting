ad10e8479e975be797737f86f824fd87339c4e6f
Rob Oudendijk <rob@yr-design.biz>
2020-05-24 15:30:50 +0900

    back

--- scripts/install_grafana.sh
+++ scripts/install_grafana.sh
@@ -7,19 +7,17 @@ if rpm -q grafana; then
   exit 0
 fi
 
-wget https://dl.grafana.com/oss/master/grafana-7.1.0-4759a6d7pre.linux-amd64.tar.gz
-tar -zxvf grafana-7.1.0-4759a6d7pre.linux-amd64.tar.gz
+wget https://dl.grafana.com/oss/master/grafana-7.1.0-4759a6d7pre.x86_64.rpm
+sudo yum install grafana-7.1.0-4759a6d7pre.x86_64.rpm
 
+/usr/sbin/grafana-cli plugins install petrslavotinek-carpetplot-panel
+/usr/sbin/grafana-cli plugins install grafana-worldmap-panel
+/usr/sbin/grafana-cli plugins install grafana-googlesheets-datasource
+/usr/sbin/grafana-cli plugins install simpod-json-datasource
 
+/usr/sbin/grafana-cli --pluginUrl https://github.com/panodata/grafana-map-panel/releases/download/0.9.0/grafana-map-panel-0.9.0.zip plugins install grafana-worldmap-panel-ng
 
-sudo grafana-cli plugins install petrslavotinek-carpetplot-panel
-sudo grafana-cli plugins install grafana-worldmap-panel
-sudo grafana-cli plugins install grafana-googlesheets-datasource
-sudo grafana-cli plugins install simpod-json-datasource
+/sbin/chkconfig --add grafana-server
+/sbin/service grafana-server start
 
-sudo grafana-cli --pluginUrl https://github.com/panodata/grafana-map-panel/releases/download/0.9.0/grafana-map-panel-0.9.0.zip plugins install grafana-worldmap-panel-ng
-
-sudo chkconfig --add grafana-server
-sudo service grafana-server start
-
-sudo ls -ll -a
+ls -ll -a
