2a15b1e5718e1e6a5fac614dab8d771737e2f931
Mat Schaffer <mat@elastic.co>
2020-06-05 09:24:34 +0900

    add image renderer plugin so we can share images in slack

--- scripts/install_grafana.sh
+++ scripts/install_grafana.sh
@@ -14,6 +14,7 @@ sudo yum install grafana-7.1.0~3a9166ddpre-1.x86_64.rpm
 /usr/sbin/grafana-cli plugins install grafana-worldmap-panel
 /usr/sbin/grafana-cli plugins install grafana-googlesheets-datasource
 /usr/sbin/grafana-cli plugins install simpod-json-datasource
+/usr/sbin/grafana-cli plugins install grafana-image-renderer
 
 /usr/sbin/grafana-cli --pluginUrl https://github.com/panodata/grafana-map-panel/releases/download/0.9.0/grafana-map-panel-0.9.0.zip plugins install grafana-worldmap-panel-ng
 
