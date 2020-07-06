5ca181426d93bde39b97ad8800eaa98ea47935de
Rob Oudendijk <rob@yr-design.biz>
2020-07-06 16:33:45 +0900

    Update install_grafana.sh
    Latest build from Garfana Map plugin from https://github.com/panodata/grafana-map-panel/archive/4f4765b68b2c02ca7de2b36dcc24349b5e10ef47.zip

--- scripts/install_grafana.sh
+++ scripts/install_grafana.sh
@@ -15,7 +15,7 @@ sudo yum install grafana-7.2.0~3f6b253fpre-1.x86_64.rpm
 /usr/sbin/grafana-cli plugins install grafana-googlesheets-datasource
 /usr/sbin/grafana-cli plugins install simpod-json-datasource
 
-/usr/sbin/grafana-cli --pluginUrl https://github.com/panodata/grafana-map-panel/releases/download/0.9.0/grafana-map-panel-0.9.0.zip plugins install grafana-worldmap-panel-ng
+/usr/sbin/grafana-cli --pluginUrl https://github.com/panodata/grafana-map-panel/archive/4f4765b68b2c02ca7de2b36dcc24349b5e10ef47.zip plugins install grafana-worldmap-panel-ng
 
 /sbin/chkconfig --add grafana-server
 /sbin/service grafana-server start
