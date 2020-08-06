643584b466a23b100d9d0f5afbe505f96bb79964
Mat Schaffer <mat@elastic.co>
2020-08-06 17:21:18 +0900

    Starting a github action to replace package.sh/py

---
+++ .github/bundle-application/entrypoint.py
@@ -0,0 +1,60 @@
+import os
+import logging
+import ebcli.core.fileoperations as fileoperations
+import json
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+def replace_docker_images():
+    container_image_prefix = 'INPUT_CONTAINER_IMAGE_'
+
+    image_replacements = {
+        k.replace(container_image_prefix, '').lower(): v
+        for k, v in os.environ.items()
+        if k.startswith('INPUT_CONTAINER_IMAGE')
+    }
+
+    with open('Dockerrun.aws.json') as f:
+        data = json.load(f)
+        for container_definition in data['containerDefinitions']:
+            name = container_definition['name']
+            if name in image_replacements.keys():
+                container_definition['image'] = image_replacements[name]
+        updated = json.dumps(data, indent=4, sort_keys=True)
+
+    with open('Dockerrun.aws.json', 'w') as f:
+        f.write(updated)
+
+
+def build_bundle(file_name):
+    file_path = fileoperations.get_zip_location(file_name)
+    logging.info('Packaging application to %s', file_path)
+    ignore_files = fileoperations.get_ebignore_list()
+    fileoperations.io.log_info = lambda message: logging.debug(message)
+    fileoperations.zip_up_project(file_path, ignore_list=ignore_files)
+
+
+def build_filename():
+    app = os.environ['INPUT_APP']
+    ref = os.environ['GITHUB_REF']
+    clean_branch_name = ref.replace('refs/', '').replace('heads/', '').replace('/', '_')
+    build_number = os.environ['GITHUB_RUN_NUMBER']
+    git_sha = os.environ['GITHUB_SHA']
+    return "{}-{}-{}-{}.zip".format(app, clean_branch_name, build_number, git_sha)
+
+
+def upload_bundle(file_name):
+    bucket = os.environ['INPUT_S3_BUCKET']
+    pass
+
+
+def main():
+    replace_docker_images()
+    file_name = build_filename()
+    build_bundle(file_name)
+    upload_bundle(file_name)
+
+
+if __name__ == "__main__":
+    main()
